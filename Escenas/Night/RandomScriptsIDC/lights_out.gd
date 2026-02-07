3790935f2f9fc88e6c7cddf6919f385a2de05a5b
Daniel Blanco <245731154+NakslolDev@users.noreply.github.com>
2026-02-07 14:09:48 +0100

    0.2.5.a Fixed lights shouting (now it is easter egg. Well, it always has been, but it was activating when it shouldnt)

--- Escenas/Night/RandomScriptsIDC/lights_out.gd
+++ Escenas/Night/RandomScriptsIDC/lights_out.gd
@@ -1,23 +1,27 @@
 extends Node2D
 
+@export var oficina_lights_out_alpha: Sprite2D
+@export var irritating_light: AudioStreamPlayer
+
+
 @export var light_out_custom := false
 @export_range(0.0, 1.0, 0.01)
 var oscuridad := 0.2
 
 func _ready():
 	visible = true
-	$Oficina_lights_out_Alpha.modulate.a = oscuridad
+	oficina_lights_out_alpha.modulate.a = oscuridad
 	Global.connect("energia_actualizada", Callable(self, "energia_act"))
-	$Irritating_light.play()
+	irritating_light.play()
 
 func energia_act():
 	if Global.energia["Luces"] or light_out_custom:
-		$Oficina_lights_out_Alpha.modulate.a = oscuridad
-		if randi_range(0, 1000) == 0:
-			$Irritating_light.volume_db = 20.0
+		oficina_lights_out_alpha.modulate.a = oscuridad
+		if randi_range(0, 1000) == 0 and irritating_light.stream_paused: #por que cojones tengo esto aqui?????? xd?
+			irritating_light.volume_db = 20.0
 		else:
-			$Irritating_light.volume_db = 3.0 #arreglar loop
-		$Irritating_light.stream_paused = false
+			irritating_light.volume_db = 3.0
+		irritating_light.stream_paused = false
 	else:
-		$Oficina_lights_out_Alpha.modulate.a = 0.5
-		$Irritating_light.stream_paused = true
+		oficina_lights_out_alpha.modulate.a = 0.5
+		irritating_light.stream_paused = true
